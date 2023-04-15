91e43fd7e57afaa1c521ec54ebf1956b285f3be8
Jcolombo <juanicolombo8@gmail.com>
2023-04-15 20:02:22 -0300

    front basico signin

--- mclatam/src/components/auth/SignIn.js
+++ mclatam/src/components/auth/SignIn.js
@@ -1,18 +1,25 @@
 import React, {useState} from 'react'
-
+import './SignIn.css'
 export const SignIn = () => {
+
+    // Estados de los inputs
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
   return (
     <div className='sign-in-container'>
         <form className='sign-in-form'>
             <h2 className='sign-in-title'>Sign In</h2>
             <div className='sign-in-inputs'>
-
-                <label htmlFor='email' placeholder='Enter your email'>Email</label>
-                <input type='email' id='email' />
-
-                <label htmlFor='password' placeholder='Enter your password'>Password</label>
-                <input type='password' id='password' />
-
+                <div className='input-container'>
+                    <label htmlFor='email' >Email</label>
+                    <input type='email' id='email'  placeholder='Enter your email' value={email}/>
+                </div>
+                <div className='input-container'>
+                    <label htmlFor='password' >Password</label>
+                    <input type='password' id='password' placeholder='Enter your password' value={password} />
+                </div>
+               
                 <button className='sign-in-button'>Sign In</button>
 
             </div>
