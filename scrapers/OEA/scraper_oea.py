9121df7aad379843576401532132789ff4a8436a
Micky Conca <mickyconca@hotmail.com>
2023-04-29 16:19:46 -0300

    scraper terminado

--- scrapers/OEA/scraper_oea.py
+++ scrapers/OEA/scraper_oea.py
@@ -4,15 +4,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from datetime import datetime
-from datetime import date, timedelta
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from twocaptcha import TwoCaptcha
 import time
-import os
-import sys
+
+ANIO_INVALIDO = 2022
 
 
 def main():
@@ -20,7 +14,7 @@ def main():
 
     # Opciones Chromedriver
     options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
+    options.add_argument('headless')
     options.add_argument("start-maximized")
     options.add_experimental_option('prefs', {
         'download.prompt_for_download': False,
@@ -42,7 +36,17 @@ def main():
 def obtener_datos_tabla(driver):
     print('Iniciando scrapeo')
 
-    while EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'Siguiente')]")):
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.ID, "status")))
+    filtro_estado = driver.find_element(By.XPATH, "//*[@id=\"status\"]")
+    Select(filtro_estado).select_by_visible_text("Abierto / En proceso")
+
+    buscar = driver.find_element(By.XPATH, "//button[contains(text(), 'Buscar')]")
+    buscar.click()
+    index = 0
+    pagina_siguiente = True
+
+    while pagina_siguiente:
         # Esperamos que cargue la tabla
         WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.ID, "offerstable")))
@@ -90,8 +94,11 @@ def obtener_datos_tabla(driver):
             print('Titulo: ' + titulo)
 
             # Obtenemos la fecha
-            fecha = datos_fila[3].text.replace('\n', ' ')
-            print('Fecha: ' + fecha)
+            fecha = datos_fila[3].text
+            anio = fecha.split('/')[2]
+            if int(anio) < ANIO_INVALIDO:
+                print('Fecha invalida')
+                pass
 
             # Obtenemos el estado de la fila
             estado = datos_fila[4].text
@@ -100,7 +107,7 @@ def obtener_datos_tabla(driver):
             print('__________________')
 
         try:
-            WebDriverWait(driver, 30).until(
+            WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Rechazar')]")))
             rechazar = driver.find_element(By.XPATH, "//button[contains(text(), 'Rechazar')]")
             driver.execute_script("arguments[0].scrollIntoView();", rechazar)
@@ -109,13 +116,18 @@ def obtener_datos_tabla(driver):
         except Exception:
             pass
 
-        WebDriverWait(driver, 30).until(
-            EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'Siguiente')]")))
-        siguiente = driver.find_element(By.XPATH, "//a[contains(text(), 'Siguiente')]")
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'Siguiente')]")))
+            siguiente = driver.find_element(By.XPATH, "//a[contains(text(), 'Siguiente')]")
+        except Exception:
+            pagina_siguiente = False
+            print("Ultima pagina")
+            break
 
         siguiente.click()
         print("click siguiente")
-        time.sleep(5)
+        time.sleep(7)
         index += 1
 
 
