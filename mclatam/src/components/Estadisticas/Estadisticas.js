1b475d857cb107d1529e9edb66424eedaf465cdc
Jcolombo <juanicolombo8@gmail.com>
2023-05-06 18:50:51 -0300

    termino estadisticas

--- mclatam/src/components/Estadisticas/Estadisticas.js
+++ mclatam/src/components/Estadisticas/Estadisticas.js
@@ -14,33 +14,10 @@ const Estadisticas = () => {
 
   // Datos para el LineChart
   const lineData = [
-    {
-      name: 'Encargado 1',
-      data: [
-        { name: 'Semana 1', value: 10 },
-        { name: 'Semana 2', value: 20 },
-        { name: 'Semana 3', value: 15 },
-        { name: 'Semana 4', value: 30 }
-      ]
-    },
-    {
-      name: 'Encargado 2',
-      data: [
-        { name: 'Semana 1', value: 5 },
-        { name: 'Semana 2', value: 15 },
-        { name: 'Semana 3', value: 10 },
-        { name: 'Semana 4', value: 20 }
-      ]
-    },
-    {
-      name: 'Encargado 3',
-      data: [
-        { name: 'Semana 1', value: 20 },
-        { name: 'Semana 2', value: 25 },
-        { name: 'Semana 3', value: 30 },
-        { name: 'Semana 4', value: 25 }
-      ]
-    }
+    { name: 'Semana 1', Encargado_1: 10, Encargado_2: 5, Encargado_3: 20 },
+    { name: 'Semana 2', Encargado_1: 20, Encargado_2: 15, Encargado_3: 25 },
+    { name: 'Semana 3', Encargado_1: 15, Encargado_2: 10, Encargado_3: 30 },
+    { name: 'Semana 4', Encargado_1: 30, Encargado_2: 20, Encargado_3: 25 },
   ];
 
   return (
@@ -55,15 +32,15 @@ const Estadisticas = () => {
   
       <h2>Cantidad de expedientes revisados por semana y por encargado</h2>
       <div className="chart-container">
-        <LineChart width={600} height={400} data={lineData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          {lineData.map((data, index) => (
-            <Line key={index} dataKey="value" data={data.data} name={data.name} stroke={`#${Math.floor(Math.random() * 16777215).toString(16)}`} />
-          ))}
-        </LineChart>
+      <LineChart width={600} height={400} data={lineData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Line dataKey="Encargado_1" stroke="#8884d8" />
+      <Line dataKey="Encargado_2" stroke="#82ca9d" />
+      <Line dataKey="Encargado_3" stroke="#ffc658" />
+    </LineChart>
       </div>
     </div>
   );
