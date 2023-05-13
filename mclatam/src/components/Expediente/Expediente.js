c63083653121e227e485c0e167167c0ad72eee51
Jcolombo <juanicolombo8@gmail.com>
2023-05-13 20:21:39 -0300

    fin modal ver datos

--- mclatam/src/components/Expediente/Expediente.js
+++ mclatam/src/components/Expediente/Expediente.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../Modal/Modal';
 import './Expediente.css';
 
 const Expediente = ({ expediente }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState('');
+
+    const handleModalOpen = (titulo, valor) => {
+        setModalContent(`${titulo}: ${valor}`);
+        setIsModalOpen(true);
+    };
+
     return (
         <div className="expediente-container">
             <h2>Expediente</h2>
             <div className="expediente-data">
                 {Object.entries(expediente).map(([titulo, valor], index) => (
-                    <div key={index} className="expediente-item">
-                        <strong>{titulo}:</strong> {valor}
+                    <div key={index} className="expediente-item" onClick={() => handleModalOpen(titulo, valor)}>
+                        <strong>{titulo}:</strong> 
+                        {valor.length > 50 
+                            ? <span>{`${valor.substring(0, 50)}...`}</span> 
+                            : valor
+                        }
                     </div>
                 ))}
             </div>
+            {
+            isModalOpen && (
+            <Modal
+              onClose={() => setIsModalOpen(false)}
+              style={{
+                alignItems: 'flex-start',
+                paddingTop: '5rem',
+                width: 'auto',
+                maxWidth: '100%',
+                minWidth: '500px',
+              }}
+            >
+              <h3>Texto completo</h3>
+              <div className="modal-fila-detalles">
+                <span className="modal-fila-detalle-valor">{modalContent}</span>
+              </div>
+            </Modal>
+            )}
         </div>
     );
 };
