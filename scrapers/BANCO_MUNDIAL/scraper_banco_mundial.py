1be427cde84060cb5182f24b666da1dd327ac2fe
Micky Conca <mickyconca@hotmail.com>
2023-08-14 14:17:22 -0400

    m

--- scrapers/BANCO_MUNDIAL/scraper_banco_mundial.py
+++ scrapers/BANCO_MUNDIAL/scraper_banco_mundial.py
@@ -22,7 +22,7 @@ def main():
 
     # Opciones Chromedriver
     options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
+    options.add_argument('headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument("start-maximized")
