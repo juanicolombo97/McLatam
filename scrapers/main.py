6db2d44d19c90c11e113d8c2ee5f5ef5b9cfeb01
mickyconca <mickyconca@hotmail.com>
2024-03-29 21:06:13 -0300

    main borra los viejos

--- scrapers/main.py
+++ scrapers/main.py
@@ -8,19 +8,21 @@ from Procurement import scraper_procurement_notices
 from Profonanpe import scraper_profonanpe
 from BID import scraper_bid
 from DevelopmentBusiness import scraper_development
+from scrapers.firebase import delete_all_documents
 
 
 def main():
     print("Ejecutando el archivo principal")
+    delete_all_documents()
     scraper_CAF.main()
     scraper_comunidad_andina.main()
     scraper_fonplata.main()
     scraper_naciones_unidas_global.main()
     scraper_oea.main()
     scraper_procurement_notices.main()
-    scraper_profonanpe.main()
+    # scraper_profonanpe.main()
     scraper_banco_mundial.main()
-    scraper_bid.main()
+    # scraper_bid.main()
     scraper_development.main()
 
 
